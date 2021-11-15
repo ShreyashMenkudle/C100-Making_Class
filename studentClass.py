5fc710df4afe2e4c646b006986f98f437e383238
ShreyashMenkudle <79455520+ShreyashMenkudle@users.noreply.github.com>
2021-11-15 17:59:02 +0530

    Add files via upload

---
+++ studentClass.py
@@ -0,0 +1,13 @@
+class Student(object):
+    def __init__(self,name,division,standard,age):
+        self.name= name
+        self.division = division
+        self.standard = standard
+        self.age = age
+
+def displayStudent(self,name):
+    print(name)
+
+stu1 = Student("Shreyash","A","9th",14)
+stu1.displayStudent("Shreyash")
+
